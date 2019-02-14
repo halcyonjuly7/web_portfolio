6dcd785f885e8e38c554acd002602269dcd195b5
Halcyon Abraham Ramirez <halcyonjuly7@gmail.com>
2019-02-13 22:56:19 -0500

    preparing for automated deployment

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+
+tar -czf project.tgz project && \
+scp package.tgz $REMOTE_USER@REMOTE_HOST:$REMOTE_DIR && \
+ssh $REMOTE_USER@REMOTE_HOST 'bash -s' < ./untar.sh
---
+++ untar.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+
+cd /root/web_portfolio
+tar -zxvf package.tgz -C . && \
+docker-compose up
