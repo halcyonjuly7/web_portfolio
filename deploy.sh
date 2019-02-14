222f859790520576e3df432815b0b3c1ffd5d3e7
Halcyon Abraham Ramirez <halcyonjuly7@gmail.com>
2019-02-14 01:58:26 -0500

    changed keys

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -x
 
 tar -czf project.tgz project && \
 scp package.tgz $REMOTE_USER@REMOTE_HOST:$REMOTE_DIR && \
-ssh $REMOTE_USER@REMOTE_HOST 'bash -s' < ./untar.sh
+ssh $REMOTE_USER@REMOTE_HOST 'bash -s ' < ./untar.sh
