da3c1e7a571a166538a11c0e7db6a56b38cc086f
Halcyon Abraham Ramirez <halcyonjuly7@gmail.com>
2019-02-16 21:15:48 -0500

    added new stuff

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -x
 
 tar -czf project.tgz project && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR && \
+scp project.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s ' < ./untar.sh
