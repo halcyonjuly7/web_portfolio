90f31a34c38287c5e0d773e76bf1c3411cbb192c
Halcyon Abraham Ramirez <halcyonjuly7@gmail.com>
2019-02-17 21:52:14 -0500

    scp docker-compose file to server

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ set -x
 
 tar -czf project.tgz project && \
 scp project.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR && \
+scp docker-compose.yml $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR &&
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s ' < ./untar.sh
