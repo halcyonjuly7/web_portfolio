32e9b9cfc321e36d2c57f93df47e717fc30a79f2
Halcyon Abraham Ramirez <halcyonjuly7@gmail.com>
2019-02-17 22:43:21 -0500

    added permission change to acme.json

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 set -x
 
 tar -czf project.tgz project && \
+tar -czf traefik.tgz traefik && \
 scp project.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR && \
-scp docker-compose.yml $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR &&
+scp traefik.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR && \
+scp docker-compose.yml $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s ' < ./untar.sh
--- untar.sh
+++ untar.sh
@@ -3,4 +3,6 @@ set -x
 
 cd /home/travis/web_portfolio &&
 tar -zxvf project.tgz  -C . && \
+tar -zxvf traefik.tgz  -C . && \
+chmod 600 ./traefik/acme.json && \
 docker-compose up -d
